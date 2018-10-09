e380d17ed7e8c7bcf3dcf5111256d5320b783df6
iDimple <wuqinyue@sjtu.edu.cn>
2018-10-09 20:02:09 +0800

    Signed-off-by: iDimple <wuqinyue@sjtu.edu.cn>

---
+++ flaskdemo/app/views.py
@@ -0,0 +1,23 @@
+from flask import  Flask,jsonify
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+@app.route("/",methods=["GET"])
+def index():
+    return jsonify({"task":tasks})
+
+if __name__=='__main__':
+        app.run(debug=True,host="0.0.0.0",port=9000)
